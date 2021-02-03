6969b3fa6aa4cac8a388b7b82773aea90f1334e8
Paul Mabileau <paulmabileau@hotmail.fr>
2021-02-03 19:13:26 +0100

    Add command shortcuts
    Not to the auto-completion however, because it would loose its interest.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- xr.sh
+++ xr.sh
@@ -6,46 +6,48 @@ _dir="$(dirname "$(readlink -f "$0")")"
 
 read -r -d '' _short_doc << EOF
 Usage: xr [<command>] [<uuid>]
-    help    Display a more complete help message.
-    clean   Remove all the downloaded student submissions.
-    notes   Print the mentoring notes for the current or given exercise.
-    edit    Open the mentoring notes in an editor.
-    test    Run the tests.
-    bench   Copy the custom benchmarks into 'benches' and run them.
+    h, help   Display a more complete help message.
+    c, clean  Remove all the downloaded student submissions.
+    n, notes  Print the mentoring notes for the current or given exercise.
+    e, edit   Open the mentoring notes in an editor.
+    t, test   Run the tests.
+    b, bench  Copy the custom benchmarks into 'benches' and run them.
 EOF
 
 read -r -d '' _long_doc << EOF
 Usage: xr [<command>] [<uuid>]
 
 Arguments:
-    command Optional subcommand, see below; when ommitted, defaults to 'test'.
-    uuid    Unique identifier for a submission: a $_uuid_len-digit hex value.
-            If a UUID is given, whatever the command used except for 'help',
-            first the corresponding exercise is downloaded using \`exercism\`
-            and the shell's current directory is changed to the returned value,
-            then the command's actual operation is executed.
+    command   Optional subcommand, see below; when ommitted, defaults to 'test'.
+    uuid      Unique identifier for a submission: a $_uuid_len-digit hex value.
+              If a UUID is given, whatever the command used except for 'help',
+              first the corresponding exercise is downloaded using \`exercism\`
+              and the shell's current directory is changed to the returned
+              value, then the command's actual operation is executed.
 
 Commands:
-    help    Display this message.
-    clean   Confirm and remove all the downloaded student submissions from the
-            filesystem using \`exercism\` in order to determine their location.
-    notes   Dump the contents of the mentoring notes for the current exercise
-            to the standard output, as deduced by reading the exercise config,
-            or from an exercise or topic of your choice through a second argument,
-            but in this case it must be explicitly prefixed with the track name
-            like so for example: \`xr notes rust/anagram\`.
-    edit    Open the mentoring notes for the current exercise in the editor
-            specified by the EDITOR environment variable, as deduced by reading
-            the exercise's configuration, or from an exercise or topic of your
-            choice through a second argument with the same rules as 'notes'.
-    test    Source the current track's script functions and run the unit tests
-            of the local exercise according to the dedicated function. This is
-            the default command when not specifying one but still giving a UUID.
-    bench   Source the current track's script functions and run the benchmarks
-            of the local exercise according to the dedicated function. It is
-            usually copying a file to its correct place and call the bencher.
-            The path of the file to copy is deduced from the exercise's local
-            \`$_exo_config\` configuration file.
+    h, help   Display this message.
+    c, clean  Confirm and remove all the downloaded student submissions from
+              the filesystem using \`exercism\` in order to determine their
+              location.
+    n, notes  Dump the contents of the mentoring notes for the current exercise
+              to the standard output, as deduced by reading the exercise config,
+              or from an exercise or topic of your choice through a second
+              argument, but in this case it must be explicitly prefixed with the
+              track name like so for example: \`xr notes rust/anagram\`.
+    e, edit   Open the mentoring notes for the current exercise in the editor
+              specified by the EDITOR environment variable, as deduced by
+              reading the exercise's configuration, or from an exercise or topic
+              of your choice through a second argument with the same rules as
+              'notes'.
+    t, test   Source the current track's script functions and run the unit tests
+              of the local exercise according to the dedicated function. This is
+              the default command when not specifying one but still giving a UUID.
+    b, bench  Source the current track's script functions and run the benchmarks
+              of the local exercise according to the dedicated function. It is
+              usually copying a file to its correct place and call the bencher.
+              The path of the file to copy is deduced from the exercise's local
+              \`$_exo_config\` configuration file.
 EOF
 
 # Returns the value associated to the given key in the exercise's configuration.
@@ -84,15 +86,15 @@ function xr() {
         return 1
     else
         case "$1" in
-            "help")
+            "h" | "help")
                 echo "$_long_doc"
             ;;
-            "clean")
+            "c" | "clean")
                 local dir="$(exercism workspace)/users" \
                     && echo "Will delete: $dir" \
                     && rm -rI "$dir"
             ;;
-            "notes" | "edit")
+            "n" | "notes" | "e" | "edit")
                 local note="$_dir/notes"
 
                 if [[ "$#" == "2" ]]; then
@@ -101,7 +103,7 @@ function xr() {
                     note="$note/$(_config_get track)/$(_config_get exercise).md"
                 fi
 
-                if [[ "$1" == "notes" ]]; then
+                if [[ ( "$1" == "n" ) || ( "$1" == "notes" ) ]]; then
                     if [[ -f "$note" ]]; then
                         cat "$note"
                     else
@@ -115,7 +117,7 @@ function xr() {
                     return 1
                 fi
             ;;
-            "test" | "bench")
+            "t" | "test" | "b" | "bench")
                 if [[ "$#" == "2" ]]; then
                     if _is_uuid "$2"; then
                         _dl_exo "$2"
@@ -126,10 +128,10 @@ function xr() {
                 fi
 
                 case "$1" in
-                    "test")
+                    "t" | "test")
                         _src_track && _run_tests
                     ;;
-                    "bench")
+                    "b" | "bench")
                         _src_track && _run_benches
                     ;;
                 esac
@@ -147,4 +149,3 @@ function xr() {
         esac
     fi
 }
-

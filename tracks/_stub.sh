860a85e2e05bd96aeb3f1228e8d6fffb9497138c
Paul Mabileau <paulmabileau@hotmail.fr>
2021-02-09 20:28:34 +0100

    Add track stub script
    Gives the basic skeleton for a track implementation.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

---
+++ tracks/_stub.sh
@@ -0,0 +1,11 @@
+# Runs all the available tests.
+function _run_tests() {
+    # Implementation here.
+}
+
+# Copies the stored benchmarks and runs them.
+function _run_benches() {
+    # Leave this and fill _ with the track's name or add an implementation.
+    echo "The _ track does not support benchmarks." >&2
+    return 1
+}

028ea1aae4ae92c5e26ad7fd2009d8827385a572
Paul Mabileau <paulmabileau@hotmail.fr>
2021-02-11 19:46:56 +0100

    Implement C# track
    No benches.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

---
+++ tracks/csharp.sh
@@ -0,0 +1,11 @@
+# Force-activates and runs all the available tests.
+function _run_tests() {
+    sed -Ei 's/(\s*\[Fact)\(Skip.*/\1\]/g' *Tests.(cs|fs) \
+        && dotnet test
+}
+
+# N/A.
+function _run_benches() {
+    echo "This .NET-based track does not support benchmarks." >&2
+    return 1
+}

e5d9897e7debe43103c37d87ab087ef96320c4b8
Paul Mabileau <paulmabileau@hotmail.fr>
2021-02-11 18:29:43 +0100

    Implement C track
    No benches.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

---
+++ tracks/c.sh
@@ -0,0 +1,12 @@
+# Runs all the available tests.
+function _run_tests() {
+    sed -Ei 's/\s*TEST_IGNORE\(\);.*//g' test/test_*.c \
+        && make -j test \
+        && make -j memcheck
+}
+
+# N/A.
+function _run_benches() {
+    echo "The C track does not support benchmarks." >&2
+    return 1
+}

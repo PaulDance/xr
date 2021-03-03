aa982c55c7e5b87d0df4d084ac1bb70bf9dde1cc
Francesco Alemanno <50984334+francescoalemanno@users.noreply.github.com>
2021-03-03 23:13:43 +0100

    Update benches/rust/forth.rs
    Co-authored-by: Paul Mabileau <35344098+PaulDance@users.noreply.github.com>

--- benches/rust/forth.rs
+++ benches/rust/forth.rs
@@ -7,7 +7,10 @@ use forth::*;
 fn simple_bench(b: &mut Bencher) {
     let mut f = Forth::new();
     b.iter(|| {
-        let x = f.eval("2 2 + : addtwo 2 + ; 2 addtwo drop drop 2 2 + : addone 1 + ; 2 addone addtwo drop drop");
-        if !x.is_ok() {panic!("bad");}
+        f.eval(
+            "2 2 + : addtwo 2 + ; 2 addtwo drop drop 2 2 + \
+            : addone 1 + ; 2 addone addtwo drop drop",
+        )
+        .unwrap();
     });
 }

52c2a5962e3ca5a637fee5d8970d28224fc60caa
Glenn Jackman <jackman@pythian.com>
2021-03-16 12:51:10 -0400

    Improvements to the bash track file

--- tracks/bash.sh
+++ tracks/bash.sh
@@ -1,6 +1,23 @@
-# Runs all the available tests.
+# Runs all the available tests, and the linter.
 function _run_tests() {
-    BATS_RUN_SKIPPED='true' bats --pretty *_test.sh
+    local ex=$(_config_get exercise)
+
+    printf "Testing %s:\n\n" "$ex"
+    if command -v bats >/dev/null; then
+        BATS_RUN_SKIPPED='true' command bats --pretty ./*_test.sh
+    else
+        echo "Installing bats-core is required"
+        echo "=> https://bats-core.readthedocs.io/en/latest/"
+        return 1
+    fi
+
+    printf "\nShellcheck:\n"
+    if command -v shellcheck >/dev/null; then
+        command shellcheck "${ex//-/_}.sh" && echo OK
+    else
+        echo "Installing shellcheck is recommended"
+        echo "=> https://github.com/koalaman/shellcheck#user-content-installing"
+    fi
 }
 
 # N/A.
